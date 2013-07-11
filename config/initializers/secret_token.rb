24d20dfbb3db7bec5c893da9acaa23d3345afa2e
olena ivashyna <olena.ivashyna@gmail.com>
2013-07-11 10:43:42 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastCV::Application.config.secret_token = '27c0fb25188464f8a9818ae873449d237fccaa2317fab51fc6c78fcc1b1e0b86220ca012497cb8f4bd4f2d36f41251d8a92e903955191dfde122a050b5600c64'
