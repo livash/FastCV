ddb99ec8c5f58dba4f5a57a1e473bff2bb6ec802
olena ivashyna <olena.ivashyna@gmail.com>
2013-07-11 14:38:32 -0700

    Installed devise user

---
+++ app/assets/javascripts/fast_cv.js
@@ -0,0 +1,13 @@
+window.FastCV = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  FastCV.initialize();
+});
